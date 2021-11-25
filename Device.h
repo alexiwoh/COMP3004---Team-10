d34eba4af5b2b158f8cf5056d67f1f25ba0119ae
jk-kevin <kevinvuongcs@gmail.com>
2021-11-25 18:14:10 -0500

    init device header file

--- Device.h
+++ Device.h
@@ -1,10 +1,49 @@
 #ifndef DEVICE_H
 #define DEVICE_H
+#include "Record.h"
+#include <QString>
 
 class Device
 {
+//MAX_RECORDS 256;
+
+private:
+    bool on;
+    bool isTouchingSkin;
+    double batteryPercentage;
+    double current; //in milliamps
+    double frequency; //0.5, 77, 100 hz
+    QString waveform;
+    int time;
+    int timeIdle; //QTimer will increment this
+    Record records[MAX_RECORDS];
+
 public:
     Device() {}
+
+    void toggle(); //on off toggle
+    void toggleTouchingSkin();
+    bool checkBattery(double);
+    void shutDown();
+
+    //Setters
+    void setBatteryPercentage(double);
+    void setFrequency(double);
+    void setCurrent(double);
+    void setWaveform(QString const);
+
+    //Getters
+    double getBattery();
+    double getFrequency();
+    double getCurrent();
+    QString getWaveform();
+
+public slots:
+    void resetTimeIdle(); //slot of the buttons
+
 };
+
+
+
 
 #endif // DEVICE_H
