e4c9b6956682ab8ff8b70037e4b99a3e587abe4e
killionptl <94575489+killionptl@users.noreply.github.com>
2021-11-25 19:01:29 -0500

    Add files via upload

---
+++ Device.cpp
@@ -0,0 +1,60 @@
+#include "Device.h"
+//testing if my uploading works  --sadiq
+Device::Device()
+{
+
+}
+
+
+//Setters
+void Device::setBatteryPercentage(){
+
+}
+
+void Device::setFrequency(){
+
+}
+
+void Device::setCurrent(){
+
+}
+
+void Device::setWaveform(){
+
+}
+
+
+
+//Getters
+double Device::getBattery(){
+
+}
+
+double Device::getFrequency(){
+
+}
+
+double Device::getCurrent(){
+
+}
+
+QString Device::getWaveform(){
+
+}
+
+//functions
+void Device::toggle(){
+
+}
+
+void Device::toggleTouchingSkin(){
+
+}
+
+bool Device::checkBattery(){
+
+}
+
+void Device::shutDown(){
+
+}
